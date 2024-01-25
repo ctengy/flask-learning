f2eef009bf2f96f0abc6c13053d787753120752d
Richard.nb <ctengy.work@gmail.com>
2024-01-26 00:19:07 +0800

    jianja render

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hellow_world():
     return render_template('index.html')
+
+@app.route('/hi/')
+def hellow_world():
+    name = {'name':'RY'}
+    return render_template('jinja.html',**name)
